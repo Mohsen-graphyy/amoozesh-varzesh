dff38cc084ac02f327627fa74bd61207e173a58e
mohsen_graphyy <piri.mohsen@ut.ac.ir>
2023-08-18 18:15:07 +0330

    ✨ add header logo

--- components/header/DesktopHeader.vue
+++ components/header/DesktopHeader.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="">slam asla</div>
+  <div class="flex flex-col w-full bg-white rounded-xl p-4">
+    <div class="flex flex-row justify-between items-center">
+      <div class="flex items-center">
+        <img src="/images/logo.svg" alt="آموزش ورزش" />
+        <h1 class="text-beta-green-100 font-bold mx-3">سامانه آموزش ورزش</h1>
+      </div>
+      <div>باتن</div>
+    </div>
+  </div>
 </template>
 
 <script>
