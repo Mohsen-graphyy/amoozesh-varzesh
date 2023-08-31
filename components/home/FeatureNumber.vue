dc0df8bad2ad93d7b5efb2163c45e988a9a86586
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-08-31 21:48:54 +0330

    ✨ impeliment feature numbers

---
+++ components/home/FeatureNumber.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div
+      class="bg-olied-100 w-12 h-12 border-4 border-beta-gray-100 rounded-full flex justify-center items-center">
+      <base-icon svg-class="stroke-white w-5" :icon-path="iconPath" />
+    </div>
+    <p class="mr-2 text-beta-green-100 font-bold">{{ number }}+</p>
+    <p class="mr-2 text-sm">{{ title }}</p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    iconPath: {
+      type: String,
+      required: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
