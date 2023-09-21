9fa64687c9724975240b5d2ac90c86631b7ad60a
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-09-21 19:21:57 +0330

    ✨ add base footer completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,102 +1,97 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-essential",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "vue"
-    ],
-    rules: {
-        'prefer-promise-reject-errors': 'off',
-        'vue/multi-word-component-names': 'off',
-        'vue/v-slot-style': 'off',
-        'vue/no-v-html': 'off',
-    
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    
-        'vue/attributes-order': [
-          'warn',
-          {
-            order: [
-              'DEFINITION',
-              'LIST_RENDERING',
-              'CONDITIONALS',
-              'RENDER_MODIFIERS',
-              'GLOBAL',
-              ['UNIQUE', 'SLOT'],
-              'OTHER_ATTR',
-              'TWO_WAY_BINDING',
-              'OTHER_DIRECTIVES',
-              'CONTENT',
-              'EVENTS',
-            ],
-            alphabetical: false,
-          },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "vue"],
+  rules: {
+    "prefer-promise-reject-errors": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/v-slot-style": "off",
+    "vue/no-v-html": "off",
+
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+
+    "vue/attributes-order": [
+      "warn",
+      {
+        order: [
+          "DEFINITION",
+          "LIST_RENDERING",
+          "CONDITIONALS",
+          "RENDER_MODIFIERS",
+          "GLOBAL",
+          ["UNIQUE", "SLOT"],
+          "OTHER_ATTR",
+          "TWO_WAY_BINDING",
+          "OTHER_DIRECTIVES",
+          "CONTENT",
+          "EVENTS",
         ],
-    
-        'vue/order-in-components': [
-          'error',
-          {
-            order: [
-              'el',
-              'name',
-              'key',
-              'parent',
-              'functional',
-              'layout',
-              'middleware',
-              'validate',
-              'scrollToTop',
-              'transition',
-              'loading',
-              'inheritAttrs',
-              ['delimiters', 'comments'],
-              ['components', 'directives', 'filters'],
-              'extends',
-              'mixins',
-              ['provide', 'inject'],
-              'ROUTER_GUARDS',
-              'model',
-              ['props', 'propsData'],
-              'emits',
-              'setup',
-              'data',
-              'computed',
-              'watch',
-              'asyncData',
-              'fetch',
-              'LIFECYCLE_HOOKS',
-              'methods',
-              'head',
-              ['template', 'render'],
-              'renderError',
-            ],
-          },
+        alphabetical: false,
+      },
+    ],
+
+    "vue/order-in-components": [
+      "error",
+      {
+        order: [
+          "el",
+          "name",
+          "key",
+          "parent",
+          "functional",
+          "layout",
+          "middleware",
+          "validate",
+          "scrollToTop",
+          "transition",
+          "loading",
+          "inheritAttrs",
+          ["delimiters", "comments"],
+          ["components", "directives", "filters"],
+          "extends",
+          "mixins",
+          ["provide", "inject"],
+          "ROUTER_GUARDS",
+          "model",
+          ["props", "propsData"],
+          "emits",
+          "setup",
+          "data",
+          "computed",
+          "watch",
+          "asyncData",
+          "fetch",
+          "LIFECYCLE_HOOKS",
+          "methods",
+          "head",
+          ["template", "render"],
+          "renderError",
         ],
       },
-}
+    ],
+  },
+};
---
+++ components/footer/FooterLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col mt-10 md:flex-row md:justify-between items-center">
+    <div class="md:w-1/2">
+      <div class="flex items-center">
+        <img src="/images/logo.svg" alt="آموزش ورزش" />
+        <h1 class="font-bold mr-2">سامانه آموزش ورزش</h1>
+      </div>
+      <h2 class="text-sm mt-3">
+        در این وبسایت ما سعی کردیم در 3 بخش معرفی دوره‌های مربیگری و سمینارها،
+        زبان علوم ورزشی و لایوهای آموزشی رایگان اطلاعاتی را در اختیار دانشجویان
+        تربیت‌بدنی و مربیان عزیز قرار دهیم.
+      </h2>
+    </div>
+    <div class="flex flex-row gap-1 mt-10">
+      <div
+        v-for="item in 4"
+        :key="item"
+        class="w-14 h-14 bg-white mx-2 rounded-lg"></div>
+    </div>
+  </div>
+</template>
