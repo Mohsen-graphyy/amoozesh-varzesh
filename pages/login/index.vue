886208e616d65a4a037181f4580ba6f8cfaeb4d4
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-10-03 17:09:06 +0330

    ✨ create login page box

---
+++ pages/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="flex h-screen justify-center items-center bg-beta-gray-50">
+    <div class="bg-white rounded-xl p-5 auth-box-shadow">salka</div>
+  </section>
+</template>
+<script setup>
+defineOptions({
+  name: "login",
+});
+definePageMeta({
+  layout: false,
+});
+</script>
+<style scoped>
+.auth-box-shadow {
+  box-shadow: 0px 0px 40px -12px rgba(71, 85, 105, 0.13);
+}
+</style>
--- pages/profile/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>salta</div>
-</template>
-<script setup>
-definePageMeta({
-  name: "profile",
-});
-</script>
