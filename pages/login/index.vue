b15e1b9de349eaaa4ee7de3fa76ee1fd1abcb0cb
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-10-20 21:14:11 +0330

    💄 change login container

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,11 +1,17 @@
 <template>
   <section class="flex h-screen justify-center items-center bg-beta-gray-50">
-    <div
-      class="flex flex-col items-center bg-white rounded-xl p-5 auth-box-shadow">
-      <div class="flex justify-center items-center">
-        <base-logo title-classes=" font-bold" />
+    <div class="relative">
+      <base-icon
+        class="w-20 absolute -top-10 -z-10 -right-10"
+        icon-path="Booltan"
+        svg-class="w-20" />
+      <div
+        class="flex flex-col items-center bg-white rounded-xl p-5 auth-box-shadow">
+        <div class="flex justify-center items-center">
+          <base-logo title-classes=" font-bold" />
+        </div>
+        <nuxt-link :to="{ name: 'home' }">bro home</nuxt-link>
       </div>
-      <nuxt-link :to="{ name: 'home' }">bro home</nuxt-link>
     </div>
   </section>
 </template>
