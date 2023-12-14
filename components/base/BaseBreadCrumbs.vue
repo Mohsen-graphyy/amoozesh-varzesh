90dacdba8bc83c091f59c7b5f06cfe90c6ad5c55
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-12-14 19:07:21 +0330

    ✨ create book inside page

--- components/base/BaseBreadCrumbs.vue
+++ components/base/BaseBreadCrumbs.vue
@@ -1,32 +1,34 @@
 <template>
-  <ul class="flex items-center">
-    <li class="flex items-center">
-      <BaseIcon
-        icon-path="Home"
-        svg-class="w-4"
-        class="stroke-beta-green-100 ml-2" />
-      <nuxt-link :to="{ name: 'home' }" class="text-beta-gray-150"
-        >صفحه اصلی</nuxt-link
-      >
-    </li>
-    <li
-      v-for="(crumb, index) in crumbsRoute"
-      :key="crumb.name"
-      class="flex items-center gap-2">
-      <base-icon
-        icon-path="Arrow"
-        svg-class="w-4"
-        class="stroke-beta-gray-100"></base-icon>
-      <nuxt-link
-        :to="{ name: crumb.name }"
-        class="text-beta-gray-150"
-        :class="{ 'text-beta-gray-500': index === crumbsRoute.length - 1 }"
-        >{{
-          crumb.meta.breadcrumbs ? crumb.meta.breadcrumbs : extraCrumbs
-        }}</nuxt-link
-      >
-    </li>
-  </ul>
+  <div class="overflow-x-auto relative whitespace-nowrap">
+    <ul class="flex flex-row items-center w-full h-20 md:h-auto">
+      <li class="inline-flex items-center">
+        <BaseIcon
+          icon-path="Home"
+          svg-class="w-4"
+          class="stroke-beta-green-100 ml-2" />
+        <nuxt-link :to="{ name: 'home' }" class="text-beta-gray-150"
+          >صفحه اصلی</nuxt-link
+        >
+      </li>
+      <li
+        v-for="(crumb, index) in crumbsRoute"
+        :key="crumb.name"
+        class="inline-flex items-center gap-2">
+        <base-icon
+          icon-path="Arrow"
+          svg-class="w-4"
+          class="stroke-beta-gray-100"></base-icon>
+        <nuxt-link
+          :to="{ name: crumb.name }"
+          class="text-beta-gray-150"
+          :class="{ 'text-beta-gray-500': index === crumbsRoute.length - 1 }"
+          >{{
+            crumb.meta.breadcrumbs ? crumb.meta.breadcrumbs : extraCrumbs
+          }}</nuxt-link
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 <script setup>
 const route = useRoute();
--- pages/book-store/[id].vue
+++ pages/book-store/[id].vue
@@ -1,18 +1,102 @@
 <template>
   <section class="!py-32 flex flex-col mx-4 md:mx-20">
-    <BaseBreadCrumbs :extra-crumbs="$route.params.id" />
+    <BaseBreadCrumbs :extra-crumbs="bookInfo.slug" />
     <div
-      class="flex flex-col lg:flex-row justify-between lg:items-center gap-10 mt-10"></div>
+      class="flex flex-col lg:flex-row-reverse justify-between lg:items-start gap-4 mt-10">
+      <div class="flex flex-col lg:basis-2/6 gap-4 bg-white p-4 rounded-lg">
+        <NuxtImg
+          :src="bookInfo.image"
+          width="313"
+          height="313"
+          class="w-full h-full object-cover rounded-lg" />
+        <div class="flex flex-col bg-beta-gray-50 rounded-lg px-4">
+          <div
+            v-for="bookContent in bookContents"
+            :key="bookContent.id"
+            class="flex flex-row items-center border-b border-beta-gray-100 py-4 text-sm">
+            <BaseIcon
+              :icon-path="bookContent.icon"
+              svg-class="w-4 stroke-beta-gray-300" />
+            <p class="text-beta-gray-300 mr-2">
+              {{ bookContent.label }} :
+              <span class="font-semibold">{{ bookContent.value }}</span>
+            </p>
+          </div>
+          <div class="mt-10">
+            <div class="flex flex-row justify-between items-center">
+              <p
+                class="bg-red-500 p-2 font-bold text-white rounded-lg text-center">
+                10%
+              </p>
+              <div class="flex flex-col items-end">
+                <p class="text-beta-green-100 font-semibold text-2xl">30,000</p>
+                <div class="flex flex-row items-center justify-between gap-5">
+                  <p class="text-beta-gray-150 line-through">35,000</p>
+                  <p class="text-beta-gray-300">تومان</p>
+                </div>
+              </div>
+            </div>
+            <BaseButton
+              class="my-4"
+              custom-class="bg-olied-100 text-white w-full p-4 rounded-md"
+              title="خرید دوره" />
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col lg:basis-4/6 bg-white p-4 rounded-lg gap-6">
+        <div>
+          <h1 class="font-bold text-xl mb-1">{{ bookInfo.title }}</h1>
+          <BaseIcon icon-path="BlueCircleDivider" />
+        </div>
+        <div>star</div>
+        <div class="flex items-center">
+          <BaseIcon icon-path="Book" svg-class="stroke-beta-gray-300 w-4 h-4" />
+          <p class="text-beta-gray-300">
+            حوزه کتاب :
+            <span class="text-olied-100 font-semibold">{{
+              bookInfo.type
+            }}</span>
+          </p>
+        </div>
+        <BookItems :book-items="bookInfo.writers" title="نویسندگان" />
+        <BookItems :book-items="bookInfo.bookDetails" title="محتوای کتاب" />
+      </div>
+    </div>
   </section>
 </template>
 
 <script setup>
 const route = useRoute();
-const router = useRouter();
 definePageMeta({ name: "bookSelection" });
 useHead({
   title: route.params.id,
 });
-
-console.log(route);
+const bookInfo = {
+  image: "/images/book1.png",
+  slug: "فیزیولوژی و تغذیه ورزشی",
+  title: "فیزیولوژی و تغذیه ورزشی",
+  type: "آموزشی ورزشی",
+  writers: [
+    { id: 1, name: "محمد علی شیبک" },
+    { id: 2, name: "سجاد حیدری پور" },
+    { id: 3, name: "زهرا رضایی" },
+    { id: 4, name: "هانیه بهروزی" },
+  ],
+  bookDetails: [
+    { id: 1, name: "درسنامه ی جامع" },
+    { id: 2, name: "پاسخ نامه ی تحلیلی و کاملاً تشریحی" },
+    { id: 3, name: "نکات ویژه کنکوری" },
+  ],
+};
+const bookContents = [
+  { id: 1, icon: "Book", label: "جنس جلد", value: "مقوای نازک، 240 گرم" },
+  {
+    id: 2,
+    icon: "Calender",
+    label: "سال انتشار",
+    value: "۱۳۹۷",
+  },
+  { id: 3, icon: "Paper", label: "تعداد صفحات", value: "۱۵۸ صفحه" },
+  { id: 4, icon: "Trunk", label: "هزینه ارسال", value: "رایگان" },
+];
 </script>
