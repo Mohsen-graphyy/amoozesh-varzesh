23bc83ec209eafea8809085d5c25aaef9f399342
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-12-15 15:07:30 +0330

    ✨ make dynamic component for details book

--- components/book/BookDetails.vue
+++ components/book/BookDetails.vue
@@ -1,42 +1,65 @@
 <template>
   <div class="flex flex-col bg-white p-4 rounded-lg">
-    <div class="border border-beta-gray-50 p-4 rounded-lg">
-      <button class="flex items-center gap-2">
+    <div
+      class="grid grid-cols-2 md:grid-cols-4 border gap-7 border-beta-gray-50 p-4 rounded-lg">
+      <button
+        v-for="detailHeader in detailHeaders"
+        :key="detailHeader.id"
+        class="relative flex items-center gap-2"
+        @click="selecetedComponent = detailHeader.component">
         <BaseIcon
-          icon-path="Book"
+          :icon-path="detailHeader.icon"
           :svg-class="
-            isflag
+            detailHeader.component === selecetedComponent
               ? 'stroke-beta-green-100 w-4 h-4'
               : 'stroke-beta-gray-500 w-4 h-4'
           " />
         <p
-          class="text-beta-gray-500"
-          :class="{ 'text-beta-green-100 font-semibold': isflag }">
-          توضیحات بیشتر
+          class="text-beta-gray-500 md:text-sm"
+          :class="{
+            'text-beta-green-100 font-semibold':
+              detailHeader.component === selecetedComponent,
+          }">
+          {{ detailHeader.title }}
         </p>
+        <div
+          v-if="detailHeader.component === selecetedComponent"
+          class="absolute w-12 h-1 rounded-b-lg -bottom-5 right-8 bg-beta-green-100"></div>
       </button>
     </div>
-    <div>
-      <BookItems
-        :book-items="setions"
-        title="سرفصل های کتاب :"
-        class="text-beta-gray-300 mt-5" />
-      <p class="mt-5 text-beta-gray-500 font-semibold">خلاصه ای از کتاب:</p>
-      <p class="text-beta-gray-300">
-        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-        از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-        سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
-        متنوع با هدف بهبود ابزارهای کاربردی می باشد.
-      </p>
-    </div>
+    <Transition name="fade">
+      <component :is="currentComponent"></component>
+    </Transition>
   </div>
 </template>
 <script setup>
 const isflag = ref(true);
 
-const setions = [
-  { id: 1, name: "درسنامه ی جامع " },
-  { id: 2, name: "نكات ویژه ی کنکوری" },
-  { id: 3, name: "پاسخ نامه ی تحلیلی و کاملاً تشریحی" },
+const detailHeaders = [
+  {
+    id: 1,
+    icon: "Document",
+    title: "توضیحات بیشتر",
+    component: "BookMoreDetails",
+  },
+  {
+    id: 2,
+    icon: "Image",
+    title: "تصاویر کتاب",
+    component: "BookImages",
+  },
 ];
+const selecetedComponent = ref("BookMoreDetails");
+const currentComponent = shallowRef(null);
+watch(
+  () => selecetedComponent.value,
+  () => {
+    currentComponent.value = defineAsyncComponent(() =>
+      import(`./../book/${selecetedComponent.value}.vue`)
+    );
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
---
+++ components/book/BookImages.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-row lg:grid lg:grid-cols-4 gap-4 py-6 overflow-x-auto">
+    <NuxtImg
+      v-for="count in 8"
+      :key="count"
+      src="/images/book1.png"
+      width="183"
+      height="183"
+      class="rounded-lg" />
+  </div>
+</template>
