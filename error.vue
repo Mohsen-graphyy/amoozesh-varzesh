7bb98ceabf30e0c5b8cf792470bd7d91b049508d
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-12-23 23:37:04 +0330

    ✨ add error changes 404 and 500

--- error.vue
+++ error.vue
@@ -1,29 +1,41 @@
 <template>
-  <div class="container bg-beta-green-100 text-white">
-    <div class="four_zero_four_bg"></div>
-    <div class="flex flex-col justify-center items-center">
-      <h1 class="font-bold container-text mt-20 md:mt-4">404</h1>
-      <p>صفحه مورد نظر پیدا نشد :(</p>
+  <div class="error-container">
+    <NuxtImg
+      :src="
+        error.statusCode === 404
+          ? '/images/error404.png'
+          : '/images/error500.png'
+      "
+      width="610px"
+      class="w-full md:w-1/3 px-5" />
+    <p v-if="error.statusCode === 404">متاسفانه صفحه موردنظر یافت نشد!</p>
+    <p v-else>همکاران ما، در حال بررسی مشکل می باشند.</p>
+    <div class="flex items-center justify-between gap-5">
+      <p
+        class="px-4 py-3 rounded-lg bg-white cursor-pointer"
+        @click="$router.go(-1)">
+        بازگشت به صفحه قبل
+      </p>
       <NuxtLink
-        class="mt-10 bg-white rounded-lg text-beta-green-100 p-2"
         :to="{ name: 'home' }"
-        >برو به خانه</NuxtLink
+        class="bg-beta-green-100 px-4 py-3 rounded-lg text-white"
+        >صفحه اصلی</NuxtLink
       >
     </div>
   </div>
 </template>
+<script setup>
+const error = useError();
+</script>
 <style scoped>
-.container {
-  width: 100%;
-  margin: 0 auto;
-  min-height: 100vh;
-}
-.container-text {
-  font-size: 5rem;
-}
-.four_zero_four_bg {
-  background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
-  height: 320px;
-  background-position: center;
+.error-container {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: auto 20px;
+  gap: 20px;
+  background-color: #f1f5f9;
 }
 </style>
