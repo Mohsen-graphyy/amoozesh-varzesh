3a08828a6a1603c2a3f5a8f55af73c1f20bac105
Mohsen Piri <piri.mohsen@ut.ac.ir>
2023-12-26 00:34:40 +0330

    ✨ add profile info

--- components/profile/ProfileLayout/ProfileLayoutContainer.vue
+++ components/profile/ProfileLayout/ProfileLayoutContainer.vue
@@ -4,7 +4,14 @@
     <div class="flex mt-5 gap-5">
       <div
         class="bg-olied-100 basis-1/4 rounded-xl p-4 flex flex-col text-white">
-        <div>loadRouteLocation,</div>
+        <div class="flex items-center gap-4">
+          <NuxtImg
+            src="/images/abaas-alidoost.png"
+            class="w-20 h-20 object-cover rounded-full border-8 border-solid border-olied-50" />
+          <div>
+            <p class="font-bold">محسن پیری</p>
+          </div>
+        </div>
         <div class="flex flex-col gap-4 mt-4">
           <div v-for="item in dashboard" :key="item.id">
             <MenuSideBarContainer :nav-links="item" />
