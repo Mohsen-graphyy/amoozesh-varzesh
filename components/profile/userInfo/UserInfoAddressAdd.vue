e1f3784e5c1eb5a8bb7e162984618f129d653224
Mohsen Piri <piri.mohsen@ut.ac.ir>
2024-01-09 02:01:28 +0330

    ♻️ refactor add modal

--- components/profile/userInfo/UserInfoAddressAdd.vue
+++ components/profile/userInfo/UserInfoAddressAdd.vue
@@ -9,21 +9,24 @@
       <p>افزودن آدرس جدید</p>
     </template>
     <template v-slot:bodyContent>
-      <div class="flex flex-col gap-4">
+      <div class="flex flex-col gap-4 mt-8 text-right">
         <BaseInput
           id="address"
           placeholder="آدرس جدید را وارد کنید"
-          icon="Person"
+          icon="Location"
           :is-lazy-validation="false"
           :rules="[ruleNotEmpty]"
           v-model="state.address" />
         <BaseInput
           id="postcode"
-          placeholder="آدرس جدید را وارد کنید"
-          icon="Person"
+          placeholder="کدپستی را وارد کنید"
+          icon="Home"
           :is-lazy-validation="false"
           :rules="[ruleNotEmpty]"
           v-model="state.postcode" />
+        <BaseButton
+          title="ثبت آدرس جدید"
+          custom-class="transition duration-150 hover:bg-olied-100 bg-olied-50 w-full text-white" />
       </div>
     </template>
   </BaseModal>
